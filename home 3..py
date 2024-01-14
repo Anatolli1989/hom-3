51797c0cb06210bf90444f7c2bc58184927a3f0d
DESKTOP-E6BUTSB\Анатолий <tolya.ponomarenko.89@ukr.net>
2024-01-14 20:11:11 +0300

    1 task

---
+++ home 3..py
@@ -0,0 +1,15 @@
+# q = int(input("Enter day: "))
+# if q == 1:
+#     print("Monday")
+# elif q == 2:
+#     print("Tuesday")
+# elif q == 3:
+#     print("Wednesday")
+# elif q == 4:
+#     print("Thursday")
+# elif q == 5:
+#     print("Friday")
+# elif q == 6:
+#     print("Saturday")
+# else:
+#     print("Sunday")
